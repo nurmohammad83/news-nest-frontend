5055cde30e8057d244301ba28081c49a199235ee
Nur Mohammad <mohammadnur5480@gmail.com>
2023-10-23 12:13:59 +0600

    feat: added internationgal news  page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import International from "@/components/Home/International"
 import LatestNews from "@/components/Home/LatestNews"
 import SubHeader from "@/components/SubHeader"
 import { getAllNews } from "@/services/news/get-all-news"
+import { INews } from "@/types/common"
 
 const Homepage =async () => {
-  const result = await getAllNews()
-  const bdNews = result.filter(news=>news.category === 'bangladesh')
-  const international = result.filter(news=>news.category === 'international')
-  console.log(result)
+  const result= await getAllNews() as any
+  const bdNews = result.filter((news:INews)=>news.category === 'bangladesh')
+  const international = result.filter((news:INews)=>news.category === 'international')
  return (
   <div>
     <SubHeader />
